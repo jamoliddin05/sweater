d3edf08d95d3cbf7f351cfcde787d4625fc47e6f
jamoliddin05 <jamol05@icloud.com>
2023-11-27 14:56:53 +0500

    Enabled file upload

--- src/main/java/com/example/sweater/controllers/MessageController.java
+++ src/main/java/com/example/sweater/controllers/MessageController.java
@@ -4,18 +4,27 @@ import com.example.sweater.models.Message;
 import com.example.sweater.models.User;
 import com.example.sweater.repos.MessageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
 public class MessageController {
 
     private final MessageRepository messageRepo;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/messages")
     public String mainPage(Model model,
@@ -39,8 +48,26 @@ public class MessageController {
             @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag,
-            Model model) {
+            @RequestParam("file") MultipartFile file,
+            Model model
+    ) throws IOException {
         Message message = new Message(text, tag, user);
+
+        if(file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+
+            if(!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            String uuidFile =  UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+            message.setFilename(resultFilename);
+        }
+
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
--- src/main/java/com/example/sweater/models/Message.java
+++ src/main/java/com/example/sweater/models/Message.java
@@ -22,6 +22,8 @@ public class Message {
     @JoinColumn(name = "user_id")
     private User author;
 
+    private String filename;
+
     public Message(String text, String tag, User user) {
         this.author = user;
         this.text = text;
