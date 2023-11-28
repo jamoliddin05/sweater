955a6a16ea4afb860b7e90ced771ee8711edba9b
jamoliddin05 <jamol05@icloud.com>
2023-11-28 19:09:27 +0500

    Fix a bug, and use this commit as a checkpoint

--- src/main/java/com/example/sweater/models/Message.java
+++ src/main/java/com/example/sweater/models/Message.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 public class Message {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String text;
