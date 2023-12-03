0b723970a0067de0b070b3f2dd49b25ee13d6bc2
jamoliddin05 <jamol05@icloud.com>
2023-12-03 20:03:16 +0500

    Testing a commit

--- src/main/java/com/example/sweater/models/User.java
+++ src/main/java/com/example/sweater/models/User.java
@@ -40,6 +40,9 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Message> messages;
+
     public boolean isAdmin() {
         return roles.contains(Role.ADMIN);
     }
